15afa3b4a669f578e1ee81ca03b1530bd534bf88
asadishaq15 <asadishaq392@gmail.com>
2024-03-17 14:29:37 +0500

    add page not found Page

---
+++ src/Pages/PageNotFound/pageNotFound.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './pageNotFound.css'; // Import CSS file for styling
+
+const PageNotFound = () => {
+  return (
+    <div className="not-found-container">
+      <div className="content">
+        <h1 className="title">404 - Page Not Found</h1>
+        <p className="description">
+          Oops! The page you are looking for does not exist.
+        </p>
+        <Link to="/" className="link">
+          Go back to home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
