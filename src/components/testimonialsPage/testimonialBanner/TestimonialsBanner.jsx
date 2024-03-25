0a01197b5a8e4e45534a87a235d29ad5bd25d502
asadishaq15 <asadishaq392@gmail.com>
2024-03-26 02:46:47 +0500

    resolve path issue

--- src/components/testimonialsPage/testimonialBanner/TestimonialsBanner.jsx
+++ src/components/testimonialsPage/testimonialBanner/TestimonialsBanner.jsx
@@ -2,37 +2,46 @@ import React from 'react';
 import { motion } from 'framer-motion'; // Import motion from Framer Motion
 import './TestimonialsBanner.css'; // Import the CSS file for styling
 import Clients from '../../clients/clients';
+import testimonialBackground from "../../../assets/home-testimonial-background.jpg"
 
 const TestimonialsBanner = () => {
   return (
-    <section className="testimonials-banner">
-      <div className="banner-content">
-        {/* Use motion.div for animations */}
-        <motion.h2 
-          initial={{ opacity: 0, y: -20 }} 
-          animate={{ opacity: 1, y: 0 }} 
-          transition={{ delay: 0.9 }} 
-          className="banner-heading" 
-        >
-          Testimonials
-        </motion.h2>
-        {/* Use motion.p for animations */}
-        <motion.p
-          initial={{ opacity: 0, y: -20 }} 
-          animate={{ opacity: 1, y: 0 }} 
-          transition={{ delay: 1.5 }} 
-          className="banner-paragraph"
-        >
-          Our clients are so much satisfied by working with us. The 100% satisfaction ratio and tremendous respect showcase the affection of clients towards the company. Each client is like a star in the constellation, without it the beauty is incomplete.
-        </motion.p>
-      </div>
-      <div className="clients-container">
-        <Clients />
-      </div>
-      <div className="wave wave1"></div>
-      <div className="wave wave2"></div>
-      <div className="wave wave3"></div>
-    </section>
+    <section className="testimonials-banner" style={{
+      
+    backgroundImage: `url(${testimonialBackground})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundAttachment: 'fixed',
+    backgroundRepeat: 'no-repeat'
+    
+    }}>
+    <div className="banner-content">
+      {/* Use motion.div for animations */}
+      <motion.h2 
+        initial={{ opacity: 0, y: -20 }} 
+        animate={{ opacity: 1, y: 0 }} 
+        transition={{ delay: 0.9 }} 
+        className="banner-heading" 
+      >
+        Testimonials
+      </motion.h2>
+      {/* Use motion.p for animations */}
+      <motion.p
+        initial={{ opacity: 0, y: -20 }} 
+        animate={{ opacity: 1, y: 0 }} 
+        transition={{ delay: 1.5 }} 
+        className="banner-paragraph"
+      >
+        Our clients are so much satisfied by working with us. The 100% satisfaction ratio and tremendous respect showcase the affection of clients towards the company. Each client is like a star in the constellation, without it the beauty is incomplete.
+      </motion.p>
+    </div>
+    <div className="clients-container">
+      <Clients />
+    </div>
+    <div className="wave wave1"></div>
+    <div className="wave wave2"></div>
+    <div className="wave wave3"></div>
+  </section>
   );
 };
 
